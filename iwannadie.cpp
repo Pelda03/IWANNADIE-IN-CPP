caa602ac33f21b9eedd94800b6445cbb1cd169f5
Pelda03 <110687291+Pelda03@users.noreply.github.com>
2023-09-07 13:59:04 +0200

    Update iwannadie.cpp
    change the spacing, so it's more visible

--- iwannadie.cpp
+++ iwannadie.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "II       W   W    A    N   N   N   N    A      DDDD   III   EEEEE\n";
-    std::cout << "II       W   W   A A   NN  N   NN  N   A A     D   D   I    E\n";
-    std::cout << "II       W W W  A   A  N N N   N N N  A   A    D   D   I    EEEE\n";
-    std::cout << "II       W W W  AAAAA  N  NN   N  NN  AAAAA    D   D   I    E\n";
-    std::cout << "II        W W   A   A  N   N   N   N  A   A    DDDD   III   EEEEE\n";
+    std::cout << "  II       W   W    A    N   N   N   N    A        DDDD   III   EEEEE\n";
+    std::cout << "  II       W   W   A A   NN  N   NN  N   A A       D   D   I    E\n";
+    std::cout << "  II       W W W  A   A  N N N   N N N  A   A      D   D   I    EEEE\n";
+    std::cout << "  II       W W W  AAAAA  N  NN   N  NN  AAAAA      D   D   I    E\n";
+    std::cout << "  II        W W   A   A  N   N   N   N  A   A      DDDD   III   EEEEE\n";
     return 0;
 }
